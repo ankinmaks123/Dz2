66a19b740c03e230d630c9863e6ddcf524a1557b
MAX <ankin.maks@mail.ru>
2022-05-19 19:02:11 +0300

    gotovoe dz

---
+++ Dz2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите 3 цифры!");
+while(true)
+    {string koleso =Console.ReadLine();
+        if  (koleso [2] > -1)
+            Console.WriteLine("{0}вот третья =>{1}",koleso,koleso [2]);
+        else 
+            Console.WriteLine("3 цифры нет");
+            break;
+            
+        }
+Console.WriteLine("Exit");
---
+++ Dz3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите номер дня недели!");
+int aboba = Console.Read(); 
+while (true)
+    {if (aboba>5 && aboba < 7) {
+        Console.WriteLine("Выходной");}
+    else if (aboba >7)
+       { Console.WriteLine("Введите цифру от 1 до 7");}
+    else
+        {Console.WriteLine("Это рабочий деь");
+        }
+    
+        break;
+    }
+    Console.WriteLine ("Exit");
